f506d6192f14a9acaa878133c41b527131d0cec4
Alex Ballera <alexballera@gmail.com>
2020-03-30 10:27:36 -0300

    Firebase y servicios REST

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
+import { UsuarioModel } from 'src/app/models/usuario.models';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  usuario: UsuarioModel;
+
   constructor() { }
 
   ngOnInit() {
+    this.usuario = new UsuarioModel();
+  }
+
+  onSubmit(form: NgForm) {
+
+    if (form.invalid) {
+      console.log(form.errors);
+      return;
+    }
+    console.log('Formulario enviado');
+    console.log(form);
   }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { UsuarioModel } from 'src/app/models/usuario.models';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private apiKey = 'AIzaSyC0UKCx2qn9dUeZE1MvHu9REz2dWtL27Xc';
+  // Crear Nuevo Usuario
+  // signUp?key=[API_KEY]
+
+  // LogIn
+  // signInWithPassword?key=[API_KEY]
+
+
+  constructor(private http: HttpClient) {}
+
+  logout() {}
+
+  login(usuario: UsuarioModel) {}
+
+  nuevoUsuario(usuario: UsuarioModel) {}
+
+}
